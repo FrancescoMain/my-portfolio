2f2f603937fc14f34d3574461f73724cf523e980
Francesco <cesaranofrancescomain@gmail.com>
2023-02-21 16:48:25 +0100

    main layout

---
+++ resources/views/layouts/main-layout.blade.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>My Portfolio</title>
+
+    <!-- CSRF Token -->
+    <meta name="csrf-token" content="{{ csrf_token() }}">
+
+    <!-- Usando Vite -->
+    @vite(['resources/js/app.js'])
+
+    @yieald ('head')
+</head>
+<body>
+    @include('components.header')
+
+    @include('components.footer')
+    
+</body>
+</html>
