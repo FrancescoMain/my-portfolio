307628934f178a573d660588da4329ec0231ffa2
Francesco <cesaranofrancescomain@gmail.com>
2023-02-23 14:34:29 +0100

    home

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -3,11 +3,14 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\Project;
 
 class MainController extends Controller
 {
     public function home() {
-        return view('pages.home');
+        $projects = Project :: all();
+
+        return view('pages.home' , compact('projects'));
     }
 
     public function privateHome(){
--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -2,6 +2,16 @@
 
 @section('content')
 
-    <h1>content</h1>
+    <h1>Project</h1>
+    @foreach ($projects as $project)
+        <li>
+            <h2>
+                {{$project -> name}}
+            </h2>
+            <img src="{{$project -> image}}" alt="">
+            <div>{{$project -> relase_date}}</div>
+        </li>
+        
+    @endforeach
     
 @endsection
