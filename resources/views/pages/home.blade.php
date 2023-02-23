14cb76fa945a41b6052bb3d9b972471b5fe47d06
Francesco <cesaranofrancescomain@gmail.com>
2023-02-23 16:35:43 +0100

    auth tips

--- resources/views/pages/home.blade.php
+++ resources/views/pages/home.blade.php
@@ -1,10 +1,13 @@
 @extends('layouts.main-layout')
 
 @section('content')
-
-    <a href="{{route('admin.project.create')}}">CREATE NEW PROJECT</a>
-
     <h1>Project</h1>
+
+    @auth
+    <a href="{{route('admin.project.create')}}">CREATE NEW PROJECT</a>
+        
+    @endauth
+
     @foreach ($projects as $project)
         <li>
             <a href="{{route('project.show' , $project)}}">
